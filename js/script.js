907fccac214385b40312caf07e271bd9f91ad9ef
Ilir Hamitaj <ilirhamitaj@gmail.com>
2016-06-03 10:33:50 +0200

    added js effect to pricing table

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ function logOut () {
 } 
 
 
+function zoomIn(x) {
+    x.style.webkitTransform="scale(1.1)";
+    x.style.backgroundColor="lightgray";
+   
+    
+}
+
+function zoomOut(x) {
+   x.style.webkitTransform="scale(1)"
+  x.style.backgroundColor="white";
+}
